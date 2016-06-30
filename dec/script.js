de5ecbfc1f9519182548de8c653cfad2c82787ca
Jeremy Laskar <webrender@gmail.com>
2016-06-30 09:54:03 -0700

    change github urls to HTTPS

--- dec/script.js
+++ dec/script.js
@@ -1,5 +1,5 @@
 function messageHandler(msg) {
-	if (msg.origin === "http://disqus.loc" || msg.origin === 'http://webrender.github.io')
+	if (msg.origin === "http://disqus.loc" || msg.origin === 'https://webrender.github.io')
 		var data = msg.data.split(':');
 		document.querySelector('#comment_iframe_'+data[0]).height=data[1];
 }
