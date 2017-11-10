d21ff17bceafaae70c8037effe8cd143e5721bee
Jeremy Laskar <webrender@gmail.com>
2017-11-10 15:54:16 -0800

    clear out this repo for use

--- dec/script.js
+++
@@ -1,7 +0,0 @@
-function messageHandler(msg) {
-	if (msg.origin === "http://disqus.loc" || msg.origin === 'https://webrender.github.io')
-		var data = msg.data.split(':');
-		document.querySelector('#comment_iframe_'+data[0]).height=data[1];
-}
-
-window.addEventListener("message", messageHandler, false);
